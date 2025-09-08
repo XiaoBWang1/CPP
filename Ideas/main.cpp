1c4628a32432c0a3a6e94a08bc9d7c17445f7e25
xbw24 <xbw24@yahoo.com>
2025-09-07 21:39:22 -0400

    C1

---
+++ Ideas/main.cpp
@@ -0,0 +1,13 @@
+//add input output stream  header file
+#include <iostream>
+
+/*main part of the program
+ * << : left shift operator, :: = scope resolution, std = standard, cin/cout : character input/ output
+ * data types: double(decimal), int(whole number), char(single character/ ''), bool(true/false), std::string =  "Sequence of texts ",
+ * string literal/ con(constant/read-only),
+ */
+int main(){
+    std::cout << "I love sushi!";
+
+    return 0;
+}
