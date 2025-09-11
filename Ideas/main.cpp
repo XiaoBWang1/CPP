6bb6299596876618c07aa27a72a35e600a0f8f1b
xbw24 <xbw24@yahoo.com>
2025-09-11 11:46:24 -0400

    C.1

--- Ideas/main.cpp
+++ Ideas/main.cpp
@@ -1,13 +1,15 @@
-//add input output stream  header file
+//add input output stream header file
 #include <iostream>
 
 /*main part of the program
- * << : left shift operator, :: = scope resolution, std = standard, cin/cout : character input/ output
- * data types: double(decimal), int(whole number), char(single character/ ''), bool(true/false), std::string =  "Sequence of texts ",
- * string literal/ con(constant/read-only),
+ * <</>> = left shift operator(insertion operator)/ right shift operator(extraction operator), :: = scope resolution operator, std:: = standard operation, cin/cout : character input(>>)/ output(<<)
+ * data types: double(decimal), int(whole number), char(single character/ ''), bool(true/false), std::string =  " Sequence of texts ", \n/ endl,
+ * string literal/ const(constant/read-only), overflow, truncated, namespace{x = 0}/using namespace, typedef/ using for aliases(typedef data type identifier/ using variable equals old data type)
+ * arithmetic operator(+, -, *, /), ++ | +1 | +=1 / -- | -= (increment/ decrement), *=, /=, % (modulus/ remainder), precedence(parenthesis/multiplication & division/ addition & subtraction)
+ * type conversion(implicit = automatic/ explicit = new data type), declaration/ assignment,
  */
-int main(){
-    std::cout << "I love sushi!";
 
+int main(){
+    std::cout << "I don't really like C++!";
     return 0;
 }
